bbf1d167d8ee864a1f40cee809f5e601fc1bfc49
mgoren <mgoren@aes-corp.com>
2025-10-26 18:21:48 -0400

    add tests

--- regexvm/src/test/java/com/maxgcoding/AppTest.java
+++ regexvm/src/test/java/com/maxgcoding/AppTest.java
@@ -1,8 +1,12 @@
 package com.maxgcoding;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.maxgcoding.pm.NFAType;
+import com.maxgcoding.pm.PatternMatcher;
 
 /**
  * Unit test for simple App.
@@ -12,7 +16,27 @@ class AppTest {
      * Rigorous Test.
      */
     @Test
-    void testApp() {
-        assertEquals(1, 1);
+    void testVM() {
+        String pattern = "(a*b|a+c)d";
+        NFAType type = NFAType.VIRTUAL_MACHINE;
+        List<String> testStrings = List.of("aaaabd", "abd", "aaaacd", "acd", "bd");
+        PatternMatcher pm = Match.patternMatcherFactory(pattern, type);
+        for (String str : testStrings) {
+            System.out.println("Checking " + str + " with '" + pattern + "':");
+            assertTrue(pm.match(str));
+        }
     }
+
+    @Test
+    void testPowerSet() {
+        String pattern = "(a*b|a+c)d";
+        NFAType type = NFAType.DIGRAPH;
+        List<String> testStrings = List.of("aaaabd", "abd", "aaaacd", "acd", "bd");
+        PatternMatcher pm = Match.patternMatcherFactory(pattern, type);
+        for (String str : testStrings) {
+            System.out.println("Checking " + str + " with '" + pattern + "':");
+            assertTrue(pm.match(str));
+        }
+    }
+
 }
