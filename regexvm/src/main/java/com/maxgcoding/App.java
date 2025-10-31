da3d860f7fe00ca3b736f6013ee131cf1501812b
Max Goren <mgoren@aes-corp.com>
2025-10-31 15:51:32 -0400

    fun

--- regexvm/src/main/java/com/maxgcoding/App.java
+++ regexvm/src/main/java/com/maxgcoding/App.java
@@ -1,9 +1,12 @@
 package com.maxgcoding;
 
+import java.util.List;
 import java.util.Scanner;
 
 import com.maxgcoding.regex.compile.ByteCodeCompiler;
+import com.maxgcoding.regex.compile.NFACompiler;
 import com.maxgcoding.regex.compile.parse.Parser;
+import com.maxgcoding.regex.digraph.PowerSet;
 import com.maxgcoding.regex.vm.VirtualMachine;
 
 public final class App {
@@ -14,7 +17,40 @@ public final class App {
     
     public static void main(String[] args) {
         App app = new App();
+        app.demo();
         app.repl();
+    }
+
+    public void demo() {
+        vmDemo("(a*b|ac)d", List.of("aaabd", "aaaacd", "abd", "acd", "bd", "cd"));
+        digraphDemo("(a*b|ac)d", List.of("aaabd", "aaaacd", "abd", "acd", "bd", "cd"));
+    }
+
+
+    public void vmDemo(String pattern, List<String> testStrings) {
+        VirtualMachine vm = new VirtualMachine(new ByteCodeCompiler()
+                                                    .compile(new Parser()
+                                                            .parse(pattern)));
+        testStrings.forEach(str -> {
+            if (vm.match(str)) {
+                System.out.println("Match Found!");
+            } else {
+                System.out.println("No match :(");
+            }
+        });
+    }
+
+    public void digraphDemo(String pattern, List<String> testStrings) {
+        PowerSet pm = new PowerSet(new NFACompiler()
+                                        .build(new Parser()
+                                                .parse(pattern)));
+        testStrings.forEach(str -> {
+            if (pm.match(str)){
+                System.out.println("Match Found!");
+            } else {
+                System.out.println("No match :(");
+            }
+        });
     }
 
     private void repl() {
